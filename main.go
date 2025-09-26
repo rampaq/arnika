64ddbaa2c83d982d858b2b3bd7e2b5a668d8c6d7
rampaq <tomasfaikl@seznam.cz>
2025-09-26 11:03:57 +0200

    wg modifications, support passing wg iface in down state
    - passing interface in "down" state negates situations where WG traffic
      could be sent in the window between interface creation and arnika
      setup
    - the interface can be created in "down" state by using modified
      wg-quick or by `wg setconf wg0 <(wg-quick strip "$WG_CONF")` and
      manually setting iface IP address
    - support for deactivating peer by setting a random psk

--- main.go
+++ main.go
@@ -134,7 +134,7 @@ func getPQCKey(pqcKeyFile string) (string, error) {
 	return scanner.Text(), nil
 }
 
-func setPSK(psk string, cfg *config.Config, logPrefix string) error {
+func setPSK(psk string, wgh *wg.WireGuardHandler, cfg *config.Config, logPrefix string) error {
 	if cfg.UsePQC() {
 		log.Println(logPrefix + " key derivation with PQC key enabled")
 		pQCKey, err := getPQCKey(cfg.PQCPSKFile)
@@ -147,11 +147,7 @@ func setPSK(psk string, cfg *config.Config, logPrefix string) error {
 		}
 	}
 	log.Println(logPrefix + " configure wireguard interface")
-	wireguard, err := wg.NewWireGuardHandler()
-	if err != nil {
-		return err
-	}
-	return wireguard.SetKey(cfg.WireGuardInterface, cfg.WireguardPeerPublicKey, psk)
+	return wgh.SetKey(psk)
 }
 
 func fibonacciRecursion(n int) int {
@@ -191,13 +187,17 @@ func mainLoop(cfg *config.Config) {
 	skipPush := make(chan bool)
 	kmsAuth := kms.NewClientCertificateAuth(cfg.Certificate, cfg.PrivateKey, cfg.CACertificate)
 	kmsServer := kms.NewKMSServer(cfg.KMSURL, int(cfg.KMSHTTPTimeout.Seconds()), kmsAuth)
+	wgh, err := wg.SetupWireGuardIF(cfg.WireGuardInterface, cfg.WireguardPeerPublicKey)
+	if err != nil {
+		log.Fatalf("unable to setup wg interface: %v", err)
+	}
 
-	go listenIds(cfg, skipPush, done, kmsServer)
-	go pushIds(cfg, skipPush, kmsServer)
+	go listenIds(cfg, wgh, kmsServer, skipPush, done)
+	go pushIds(cfg, wgh, kmsServer, skipPush)
 	<-done
 }
 
-func listenIds(cfg *config.Config, skipPush chan bool, done chan bool, kmsServer *kms.KMSHandler) {
+func listenIds(cfg *config.Config, wgh *wg.WireGuardHandler, kmsServer *kms.KMSHandler, skipPush chan bool, done chan bool) {
 	result := make(chan string)
 	go tcpServer(cfg.ListenAddress, result, done)
 
@@ -213,14 +213,14 @@ func listenIds(cfg *config.Config, skipPush chan bool, done chan bool, kmsServer
 			time.Sleep(time.Millisecond * 100)
 			continue
 		}
-		err = setPSK(key.GetKey(), cfg, "<-- BACKUP:")
+		err = setPSK(key.GetKey(), wgh, cfg, "<-- BACKUP:")
 		if err != nil {
 			log.Println(err.Error())
 		}
 	}
 }
 
-func pushIds(cfg *config.Config, skipPush chan bool, kmsServer *kms.KMSHandler) {
+func pushIds(cfg *config.Config, wgh *wg.WireGuardHandler, kmsServer *kms.KMSHandler, skipPush chan bool) {
 	ticker := time.NewTicker(cfg.Interval)
 	defer ticker.Stop()
 	i := 20
@@ -244,7 +244,7 @@ func pushIds(cfg *config.Config, skipPush chan bool, kmsServer *kms.KMSHandler) 
 			if err != nil {
 				log.Println(err.Error())
 			}
-			err = setPSK(key.GetKey(), cfg, "--> MASTER:")
+			err = setPSK(key.GetKey(), wgh, cfg, "--> MASTER:")
 			if err != nil {
 				log.Println(err.Error())
 			}
@@ -252,4 +252,3 @@ func pushIds(cfg *config.Config, skipPush chan bool, kmsServer *kms.KMSHandler) 
 		<-ticker.C
 	}
 }
-
